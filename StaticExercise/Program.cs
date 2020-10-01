90d7df9cbc7a836ca775c2b56694ad409dc989f9
Kevin March <marchkevin78@gmail.com>
2020-10-01 19:08:58 -0400

    FinishedStaticExercise

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(32));
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(0));
+
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(212));
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(100));
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double F = 0;
+        public static double C = 0;
+        public static double FahrenheitToCelsius(double fTemp)
+        {
+            C = (fTemp - 32) / 1.8;
+            return C;
+        }
+
+        public static double CelsiusToFahrenheit(double cTemp)
+        {
+            F = (cTemp * 9) / 5 + 32;
+            return F;
+        }
+    }
+}
